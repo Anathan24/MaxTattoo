e459fffe428bc42e6993a2af2ea266209622c630
Anatoliy Roshka <anatoliyr1995@gmail.com>
2021-06-08 17:41:58 +0200

    Deleted NotFoundException

--- src/main/java/com/maxtattoo/MaxTattooApplication.java
+++ src/main/java/com/maxtattoo/MaxTattooApplication.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootApplication
 public class MaxTattooApplication {
@@ -11,8 +13,12 @@ public class MaxTattooApplication {
 	private static Logger logger = LoggerFactory.getLogger(MaxTattooApplication.class);
 
 	public static void main(String[] args) {
-		logger.info("Hello world");
 		SpringApplication.run(MaxTattooApplication.class, args);
+
 	}
 
+//	private void httpResponceExample(){
+//		logger.info("Hello world");
+//		new ResponseEntity<>("OK", HttpStatus.OK);
+//	}
 }
