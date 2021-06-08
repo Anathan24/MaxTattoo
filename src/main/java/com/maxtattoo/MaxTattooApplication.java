0935e92c10aced19a39f15792c8bf5420b14e794
Anatoliy Roshka <anatoliyr1995@gmail.com>
2021-06-08 14:32:38 +0200

    Added slf4j to project dependency

--- src/main/java/com/maxtattoo/MaxTattooApplication.java
+++ src/main/java/com/maxtattoo/MaxTattooApplication.java
@@ -1,12 +1,17 @@
 package com.maxtattoo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaxTattooApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(MaxTattooApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Hello world");
 		SpringApplication.run(MaxTattooApplication.class, args);
 	}
 
