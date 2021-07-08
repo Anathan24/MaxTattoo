66e216969916bf42d300836e5b99e2e37bb73253
Anatoliy Roshka <anatoliyr1995@gmail.com>
2021-07-08 12:51:45 +0200

    Bug fix

--- src/main/java/com/maxtattoo/MaxTattooApplication.java
+++ src/main/java/com/maxtattoo/MaxTattooApplication.java
@@ -1,24 +1,10 @@
 package com.maxtattoo;
 
-import com.maxtattoo.service.ClientDataService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MaxTattooApplication {
-
-	private static final Logger logger = LoggerFactory.getLogger(MaxTattooApplication.class);
-
-	@Autowired
-	private final ClientDataService clientDataService;
-
-	public MaxTattooApplication(@Autowired ClientDataService clientDataService){
-		this.clientDataService = clientDataService;
-		logger.info("CLIENT: {}", clientDataService.getAllClientData(1L));
-	}
 
 	public static void main(String[] args){
 		SpringApplication.run(MaxTattooApplication.class, args);
--- src/main/java/com/maxtattoo/controller/GenericRestController.java
+++ src/main/java/com/maxtattoo/controller/GenericRestController.java
@@ -2,28 +2,31 @@ package com.maxtattoo.controller;
 
 import com.maxtattoo.command.ClientDataCommand;
 import com.maxtattoo.model.ClientModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
-@RequestMapping("client")
+@RequestMapping(produces = {MediaType.APPLICATION_JSON_VALUE, "application/hal+json"})
 public class GenericRestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(GenericRestController.class);
+
     @Autowired
-    private final BeanFactory getBean;
+    private BeanFactory getBean;
 
-    public GenericRestController(@Autowired BeanFactory beanFactory){
-        this.getBean = beanFactory;
-    }
-
-    @GetMapping(value = "/{id}", produces = "application/json")
-    public ResponseEntity<ClientModel> allClientData(@PathVariable("id") Long id){
-        ClientDataCommand clientCommand = getBean.getBean(ClientDataCommand.class);
-        var clientModel = clientCommand.execute(id);
-        return new ResponseEntity<>(clientModel, HttpStatus.OK);
+    @GetMapping(value = "/client")
+    public ResponseEntity<ClientModel> allClientData(@RequestParam Long id){
+        logger.info("Invoked allClientData method of controller");
+        ClientDataCommand clientCommand = getBean.getBean(ClientDataCommand.class, id);
+        var clientModel = clientCommand.execute();
+        return ok(clientModel);
     }
 
 }
