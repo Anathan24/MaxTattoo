bdaeec0e54520698a3e0a9f1173c99e1b3e36a1f
anatoliy.roshka <roshka.anatoliy@gmail.com>
2022-07-11 17:37:13 +0200

    Changed cross origin delete port

--- src/main/java/com/maxtattoo/controller/GenericController.java
+++ src/main/java/com/maxtattoo/controller/GenericController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin
 abstract class GenericController {
 
     final Logger logger = LoggerFactory.getLogger(this.getClass());
